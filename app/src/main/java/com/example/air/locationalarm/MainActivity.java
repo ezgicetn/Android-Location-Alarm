9bc1457dd869db6e020ffa2cb3026bc29dbf2793
Irem Nur ARI <nurirem96@hotmail.com>
2018-12-10 19:14:28 +0300

    Added Google Maps to layout

--- app/src/main/java/com/example/air/locationalarm/MainActivity.java
+++ app/src/main/java/com/example/air/locationalarm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.air.locationalarm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        listView = (ListView)findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
 
 
         ArrayList <Reminder> reminderArrayList = new ArrayList<>();
@@ -30,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(listAdapter);
 
 
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MapViewActivity.class);
+                startActivity(intent);
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
