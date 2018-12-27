48635b928adc7b863e6422fee6539d423f4cf515
Irem Nur ARI <nurirem96@hotmail.com>
2018-12-27 21:54:38 +0300

    Get Current Location

--- app/src/main/java/com/example/air/locationalarm/MainActivity.java
+++ app/src/main/java/com/example/air/locationalarm/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.air.locationalarm;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST =9001;
     ListView listView;
     List<Reminder> reminderList;
     DatabaseHelper databaseHelper;
@@ -38,8 +46,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
-
+        if(CheckServices()){
+            init();
+        }
+    }
+    private void init(){
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,26 +62,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public boolean CheckServices(){
+        Log.d(TAG, "CheckServices: Checking google services");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available== ConnectionResult.SUCCESS){
+            //Google Play Services are working
+            Log.d(TAG, "CheckServices: Google Play Services working");
             return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //There is an error, it can be fixed
+            Log.d(TAG, "CheckServices: There is an error but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this,"You can't make map requests",Toast.LENGTH_SHORT).show();
         }
+        return false;
 
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/air/locationalarm/MapViewActivity.java
+++ app/src/main/java/com/example/air/locationalarm/MapViewActivity.java
@@ -1,28 +1,59 @@
 package com.example.air.locationalarm;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-import java.util.ArrayList;
 
 public class MapViewActivity extends AppCompatActivity implements OnMapReadyCallback{
-    private MapView mapView;
-    private GoogleMap gmap;
+    private static final String TAG = "MapViewActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 123;
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+    private static final LatLngBounds AUSTRALIA = new LatLngBounds(
+            new LatLng(SYDNEY.latitude, SYDNEY.longitude), new LatLng(BRISBANE.latitude, BRISBANE.longitude));
+    private Marker mPerth;
+    private Marker mSydney;
+    private Marker mBrisbane;
+    private GoogleMap gMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static Location mLastLocation;
+    private boolean mPermissionsGranted = false;
+    private Marker marker;
     DatabaseHelper databaseHelper = new DatabaseHelper(this);
     EditText titleEdt, detail;
     Button saveBtn;
+    FloatingActionButton locBtn;
     Boolean existing = false;
     Reminder r;
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,96 +62,176 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
         titleEdt = findViewById(R.id.titleEditText);
         detail = findViewById(R.id.detailEditText);
         saveBtn = findViewById(R.id.saveBtn);
+        locBtn = findViewById(R.id.currentLocation);
         Bundle bundle = getIntent().getExtras();
-            int ID = 0;
-            if(bundle != null){
-                ID = bundle.getInt("ID");
-                r = databaseHelper.getSaved(ID);
-                titleEdt.setText(r.getTitle());
-                detail.setText(r.getDetail());
-                existing = true;
-            }
-
-
-
+        int ID = 0;
+        if (bundle != null) {
+            ID = bundle.getInt("ID");
+            r = databaseHelper.getSaved(ID);
+            titleEdt.setText(r.getTitle());
+            detail.setText(r.getDetail());
+            existing = true;
+        }
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(existing){
+                if (existing) {
                     r.setTitle(titleEdt.getText().toString());
                     r.setDetail(detail.getText().toString());
                     databaseHelper.editReminder(r);
-                }
-                else {
+                } else {
                     String title = titleEdt.getText().toString();
                     String det = detail.getText().toString();
                     Reminder reminder = new Reminder(title, det);
                     databaseHelper.addReminder(reminder);
-                    }
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
+                }
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
 
             }
         });
+        locBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+        getLocPermission();
+        /*double lat = mLastLocation.getLatitude();
+        double lng = mLastLocation.getLongitude();
+        String loc = "loc: "+lat+" , "+lng+".";
+        Toast.makeText(this, loc,Toast.LENGTH_LONG).show();*/
 
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+
+    }
+    private void getLocation() {
+        Log.d(TAG, "getDeviceLocation: getting device location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if (mPermissionsGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            Log.d(TAG, "onComplete: found location!!");
+                            Location currentLoc = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLoc.getLatitude(), currentLoc.getLongitude()), 15f,"My Location");
+
+                        } else {
+                            Log.d(TAG, "onComplete: current location not found");
+                            Toast.makeText(MapViewActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.d(TAG, "getDeviceLocation: SecurityException" + e.getMessage());
         }
-
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(mapViewBundle);
-        mapView.getMapAsync(this);
     }
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    private void getLocPermission() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
 
-        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mPermissionsGranted = true;
+                initMap();
+
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 123);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, 123);
         }
-
-        mapView.onSaveInstanceState(mapViewBundle);
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermissionsGranted = false;
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    mPermissionsGranted = true;
+                    //if permissions granted initialize map
+                    initMap();
+
+                }
+            }
+        }
     }
-    @Override
-    protected void onPause() {
-        mapView.onPause();
-        super.onPause();
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapViewActivity.this);
     }
-    @Override
-    protected void onDestroy() {
-        mapView.onDestroy();
-        super.onDestroy();
-    }
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
+    private void moveCamera(LatLng latLng, float zoom,String title) {
+        Log.d(TAG, "MoveCamera: moving camera to" + latLng);
+        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        //create and drop marker on map
+        if(title!="My Location"){
+            if(marker != null)
+                marker.remove();
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+
+            marker = gMap.addMarker(options);
+        }
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        gmap = googleMap;
-        gmap.setMinZoomPreference(12);
-        LatLng ny = new LatLng(40.7143528, -74.0059731);
-        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+       /* gMap = googleMap;
+        // Add some markers to the map, and add a data object to each marker.
+        mPerth = gMap.addMarker(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth"));
+        mPerth.setTag(0);
+
+        mSydney = gMap.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney"));
+        mSydney.setTag(0);
+
+        mBrisbane = gMap.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane"));
+        mBrisbane.setTag(0);
+        //gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(AUSTRALIA.getCenter(),1));*/
+            Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+            gMap = googleMap;
+
+            if (mPermissionsGranted) {
+                getLocation();
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                gMap.setMyLocationEnabled(true); //blue dot on map
+               // gMap.getUiSettings().setMyLocationButtonEnabled(false); //Google's find my location button
+               // gMap.getUiSettings().setMapToolbarEnabled(false); //Set default directions button false
+
+                //init();
+
+        }
+
     }
+
+
 }
